1e738cad1ad058617ec3faa6f939ea9e646bf628
saipavan desktop <saipavantej@gmail.com>
2019-11-05 18:22:22 +0530

    postitive or negative integer

---
+++ check a number is odd or even/Test.java
@@ -0,0 +1,27 @@
+// odd or even 
+import java.util.Scanner;
+class Test
+{
+    public static void main(String args[])
+    {
+        int a;
+        System.out.println(" enter a number ");
+        Scanner a1 = new Scanner(System.in);
+        a = a1.nextInt();
+          System.out.println(" ________________ ");
+         if(a<=0)
+        {
+            System.out.println("please enter the positive non zero number ");
+        }
+        else if(a%2==0)
+        {
+            System.out.println(a+" is even number");
+        }
+        else if(a%2!=0)
+        {
+             System.out.println(a+" is odd number");
+        }
+        
+        
+    }
+}
