fe929d5b9635254355643006bc9bb016251ae5bb
saipavan desktop <saipavantej@gmail.com>
2019-11-06 00:17:00 +0530

    Fibonacci series with recursion

---
+++ Fibonacci series with recursion/Test.java
@@ -0,0 +1,32 @@
+// fibonacci series with recursion
+import java.util.Scanner;
+class Test
+    
+{
+    static int fib (int n)
+    {
+        if(n==0)
+            
+        {
+            return 0;
+        }
+    if(n==1 || n==2)
+    {
+       return 1;
+        
+    }
+        return fib(n-2)+fib(n-1);
+    }
+    
+    public static void main(String args[])
+    {
+        int a,i;
+        System.out.println("enter the range ");
+        Scanner a1= new Scanner(System.in);
+        a =a1.nextInt();
+        for(i=0;i<a;i++)
+        {
+        System.out.print(fib(i)+" ");
+        }
+    }
+}
