b3aa6123743efa0ede149f113a6f7686fbe94bfa
saipavan desktop <saipavantej@gmail.com>
2019-11-05 14:59:49 +0530

    Generate Table For Any Number

---
+++ Generate Table For Any Number/Test.java
@@ -0,0 +1,17 @@
+//
+import java.util.Scanner;
+class Test 
+{
+    public static void main(String args[])
+    {
+    int a,i;
+        System.out.println("Enter the tables number");
+            Scanner a1 = new Scanner (System.in);
+            a = a1.nextInt();
+         System.out.println("---------------------------");
+        for(i=1;i<=10;i++)
+        {
+           System.out.println(i+ " * "+a +" = " +(i*a)); 
+        }
+    }
+}
