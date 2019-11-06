1cc7aa206083073321a028e4e1f5fc6794975c13
saipavan desktop <saipavantej@gmail.com>
2019-11-06 17:44:34 +0530

    the length of the string

--- find the length of a string/Test.java
+++ find the length of a string/Test.java
@@ -4,14 +4,17 @@ class Test
 {
    public static void main(String args[])
    {
-       String a;
-       int r;
+       String a,b;
+     
        
        System.out.println("enter the string");
        Scanner a1 = new Scanner(System.in);
-       a =a1.next();
-       r = a.length();
-       System.out.println("...............\n The length of the string  including space's is "+ r);
- 
+       a =a1.nextLine();
+    
+       System.out.println("...............\n The length of the string  including space's is "+ a.length());
+       
+       b =a.replace(" ","");
+      
+        System.out.println("...............\n The length of the string without including space's is "+ b.length());
    }
 }
